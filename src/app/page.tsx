bed09cbf05852c1b72cddba3361d5fce11d8b1ed
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-09-03 13:13:08 +0530

    static images add for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+import { auth, signOut } from "@/auth";
 import { Button } from "@nextui-org/react";
 import { FaRegSmile } from "react-icons/fa";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
   return (
     <main>
-      <Button 
-      color="primary" 
-      variant="bordered" 
-      startContent={<FaRegSmile size={20} />}>
-        Click me
-      </Button>
+      <form action={async () => {
+        'use server'
+
+        await signOut()
+      }}>
+        <Button
+          type="submit"
+          color="primary"
+          variant="bordered"
+          startContent={<FaRegSmile size={20} />}>
+          Click me
+        </Button>
+      </form>
     </main>
   );
 }
