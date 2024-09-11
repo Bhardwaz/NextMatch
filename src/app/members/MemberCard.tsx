01545cb51fcbafd7af5a5e3010bd8e2d2c53593a
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2024-09-11 17:37:38 +0530

    LikeButton added

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -1,3 +1,5 @@
+"use client"
+import LikeButton from '@/components/LikeButton'
 import { calculateAge } from '@/lib/util'
 import { Card, CardFooter, Image } from '@nextui-org/react'
 import { Member } from '@prisma/client'
@@ -5,10 +7,16 @@ import Link from 'next/link'
 import React from 'react'
 
 type MemberCardProps = {
-    member: Member
+    member: Member,
+    likeIds: string[]
 }
 
-const MemberCard = ({ member }: MemberCardProps) => {
+const MemberCard = ({ member, likeIds }: MemberCardProps) => {
+    const hasLiked = likeIds.includes(member.userId)
+    const preventLinkAction = (e: React.MouseEvent) => {
+        e.preventDefault()
+        e.stopPropagation()
+    }
     return (
         <Card
             fullWidth
@@ -23,6 +31,11 @@ const MemberCard = ({ member }: MemberCardProps) => {
                 src={member.image || "/images/user.png"}
                 className='aspect-square object-cover'
             />
+            <div onClick={preventLinkAction} >
+                <div className='absolute top-3 right-3 z-50'>
+                    <LikeButton targetId={member.userId} hasLiked={hasLiked} />
+                </div>
+            </div>
             <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
                 <div className='flex flex-col text-white'>
                     <span className='font-semibold'>{member.name}{calculateAge(member.dateOfBirth)}</span>
